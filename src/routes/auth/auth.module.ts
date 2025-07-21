4c67d4bf973834336ca16acf6b7b308157dad6c5
Fink2005 <phans3806@gmail.com>
2025-07-22 01:29:31 +0700

    enhance size

--- src/routes/auth/auth.module.ts
+++ src/routes/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({imports: [SharedModule],
+    // =======================
   providers: [AuthService],
   controllers: [AuthController],
 })
