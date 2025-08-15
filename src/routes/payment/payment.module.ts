3ca70dfb36f64f582ad7348881af5d33f53cbc10
Fink2005 <phans3806@gmail.com>
2025-08-16 03:51:36 +0700

    feat: migrate db

--- src/routes/payment/payment.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaymentController } from './payment.controller';
-import { PaymentService } from './payment.service';
-
-@Module({
-  providers: [PaymentService],
-  controllers: [PaymentController],
-})
-export class PaymentModule {}
